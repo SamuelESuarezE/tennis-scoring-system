a37eba0c961d4bcb5d18a6c85aa04b9ea5786b52
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2025-08-30 12:49:45 -0500

    Main example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,12 @@
-import model.Match
+import model.Game
 import model.Player
 
 fun main() {
     val playerOne = Player()
     val playerTwo = Player()
-    val match = Match(playerOne, playerTwo)
-    match.playerWinsAPoint(1)
-    match.playerWinsAPoint(2)
-    match.playerWinsAPoint(1)
-    match.playerWinsAPoint(1)
-    match.playerWinsAPoint(2)
-    match.playerWinsAPoint(2)
-    match.playerWinsAPoint(2)
-    match.playerWinsAPoint(1)
-
-
-    match.displayScore()
+    val game = Game(playerOne, playerTwo)
+    game.playerWinsAPoint(1) // 15-0
+    game.playerWinsAPoint(2) // 15-15
+    repeat(3) { game.playerWinsAPoint(1) } // 40-15
+    println(game.getScore()) // Player 1 wins the match!
 }
