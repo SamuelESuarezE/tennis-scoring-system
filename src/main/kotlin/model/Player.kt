b2caf44e46e1fbe9e2437e9bf37c847718696443
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2025-08-30 11:31:02 -0500

    Added a reset score method

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -1,13 +1,17 @@
 package model
 
 class Player(
-    val name: String,
     private var score: Int = 0,
-    var advantage: Boolean = false
+    var advantage: Boolean = false,
+    var won: Boolean = false
 ) {
     fun winsAPoint() {
         score += if (score == 30) 10 else 15
     }
 
     fun getScore(): Int = score
+
+    fun resetScore() {
+        score = 0
+    }
 }
